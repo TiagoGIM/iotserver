9ef0edbe4c26ed6f07372255327b66312b97e2b9
richard <richardsonsantiago@ufrn.edu.br>
2018-04-02 11:28:24 -0300

    coisas

--- app/routes/index.js
+++
@@ -1,5 +0,0 @@
-const home = require('../controllers/home.js');
-module.exports = (application) => {
-	application.get('/', (req, res) => home.index(application, req, res));
-	application.get('/thing',(req,res)=> home.thing(application,req,res));
-};
